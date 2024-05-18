8c081162d5c0a4efe5b860b9e89a9dc37b452299
leonHajdarevic <leon.hajdarevic@gmail.com>
2024-05-18 16:35:46 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import { slika1 } from "./img/head.jpg";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Metal Muzika</h1>
+      <img src={slika1} />
+    </div>
+  );
+}
